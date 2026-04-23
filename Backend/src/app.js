412b44f5867bc2dc1bdaba862222db48764ac7f5
Developer <developer@smartqueue.local>
2026-04-23 22:08:59 +0530

    check

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,6 +13,10 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(cookieParser())
 
+app.get('/', (req, res) => {
+    res.send("working")
+})
+
 import userRouter from "./routes/user.routers.js";
 import postRouter from "./routes/post.routers.js";
 
