03f13b0e7dc00cab500a829c1a934647e2f1570d
Developer <developer@smartqueue.local>
2026-04-24 00:13:12 +0530

    connecting4

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try{
-       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/testApp`);
+       const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     //    console.log(connectionInstance);
     } catch(err){
